9cf7461b130996ef1ce754d38832937c5f980b6d
shioeri <sheynsw.feb02@gmail.com>
2023-07-10 17:53:40 +0700

    fix: freeze issue when the `settings.ini` file is missing.

--- box/scripts/start.sh
+++ box/scripts/start.sh
@@ -1,17 +1,13 @@
 #!/system/bin/sh
 
 moddir="/data/adb/modules/box_for_root"
-if [ -n "$(magisk -v | grep lite)" ]; then
-  moddir="/data/adb/lite_modules/box_for_root"
-fi
+[ -n "$(magisk -v | grep lite)" ] && moddir="/data/adb/lite_modules/box_for_root"
 
-if [ -f "/data/adb/magisk/busybox" ]; then
-  busybox="/data/adb/magisk/busybox"
-elif [ -f "/data/adb/ksu/bin/busybox" ]; then
-  busybox="/data/adb/ksu/bin/busybox"
-fi
+busybox="/data/adb/magisk/busybox"
+[ -f "/data/adb/ksu/bin/busybox" ] && busybox="/data/adb/ksu/bin/busybox"
 
 scripts_dir="/data/adb/box/scripts"
+file_settings="/data/adb/box/settings.ini"
 
 refresh_box() {
   if [ -f "/data/adb/box/run/box.pid" ]; then
@@ -21,37 +17,43 @@ refresh_box() {
 }
 
 start_service() {
-    if [ ! -f "${moddir}/disable" ]; then
-      "${scripts_dir}/box.service" start >> "/dev/null" 2>&1
-    fi
+  if [ ! -f "${moddir}/disable" ]; then
+    "${scripts_dir}/box.service" start >> "/dev/null" 2>&1
+  fi
 }
 
 enable_iptables() {
-    PIDS=("clash" "xray" "sing-box" "v2fly")
-    PID=""
-    i=0
-    while [ -z "$PID" ] && [ "$i" -lt "${#PIDS[@]}" ]; do
-      PID=$(${busybox} pidof "${PIDS[$i]}")
-      i=$((i+1))
-    done
+  PIDS=("clash" "xray" "sing-box" "v2fly")
+  PID=""
+  i=0
+  while [ -z "$PID" ] && [ "$i" -lt "${#PIDS[@]}" ]; do
+    PID=$($busybox pidof "${PIDS[$i]}")
+    i=$((i+1))
+  done
 
-    if [ -n "$PID" ]; then
-      "${scripts_dir}/box.iptables" enable >> "/dev/null" 2>&1
-    fi
+  if [ -n "$PID" ]; then
+    "${scripts_dir}/box.iptables" enable >> "/dev/null" 2>&1
+  fi
 }
 
 start_inotifyd() {
-    for PID in $(${busybox} pidof inotifyd); do
-      if grep -q box.inotify /proc/$PID/cmdline; then
-        kill -15 ${pid}
-      fi
-    done
-    inotifyd "${scripts_dir}/box.inotify" "${moddir}" >> "/dev/null" 2>&1 &
+  PIDs=($($busybox pidof inotifyd))
+  for PID in "${PIDs[@]}"; do
+    if grep -q "box.inotify" "/proc/$PID/cmdline"; then
+      kill -9 "$PID"
+    fi
+  done
+  inotifyd "${scripts_dir}/box.inotify" "${moddir}" >> "/dev/null" 2>&1 &
 }
 
-refresh_box
-if [ ! -f "/data/adb/box/manual" ]; then
-    start_service
-    enable_iptables
-    start_inotifyd
+if [ -f "/data/adb/box/manual" ]; then
+  exit 1
 fi
+
+if [ -f "$file_settings" ] && [ -r "$file_settings" ] && [ -s "$file_settings" ]; then
+  refresh_box
+  start_service
+  enable_iptables
+fi
+
+start_inotifyd
--- customize.sh
+++ customize.sh
@@ -3,18 +3,15 @@
 SKIPUNZIP=1
 ASH_STANDALONE=1
 
-if [ "$BOOTMODE" ! = true ] ; then
+### INSTALLATION ###
+
+if [ "$BOOTMODE" != true ]; then
   ui_print "-----------------------------------------------------------"
   ui_print "! Please install in Magisk Manager or KernelSU Manager"
   ui_print "! Install from recovery is NOT supported"
   abort "-----------------------------------------------------------"
-elif [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10670 ] ; then
-  abort "ERROR: Please update your KernelSU and KernelSU Manager or KernelSU Manager"
-fi
-
-# check Magisk
-if [ "$KSU" ! = true ] ; then
-    ui_print "- Magisk version: $MAGISK_VER ($MAGISK_VER_CODE)"
+elif [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10670 ]; then
+  abort "ERROR: Please update your KernelSU and KernelSU Manager"
 fi
 
 # check android
@@ -25,26 +22,27 @@ else
   ui_print "- Device sdk: $API"
 fi
 
-if [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10683 ] ; then
-  service_dir="/data/adb/ksu/service.d"
-  busybox="/data/adb/ksu/bin/busybox"
-else 
-  service_dir="/data/adb/service.d"
-  busybox="/data/adb/magisk/busybox"
+# check version
+service_dir="/data/adb/service.d"
+if [ "$KSU" = true ]; then
+  ui_print "- kernelSU version: $KSU_VER ($KSU_VER_CODE)"
+  [ "$KSU_VER_CODE" -lt 10683 ] && service_dir="/data/adb/ksu/service.d"
+else
+  ui_print "- Magisk version: $MAGISK_VER ($MAGISK_VER_CODE)"
 fi
 
-if [ ! -d "${service_dir}" ] ; then
-    mkdir -p "${service_dir}"
+if [ ! -d "${service_dir}" ]; then
+  mkdir -p "${service_dir}"
 fi
 
 if [ -d "/data/adb/modules/box_for_magisk" ]; then
   rm -rf "/data/adb/modules/box_for_magisk"
-  ui_print "- Old module deleted."
+  ui_print "- old module deleted."
 fi
 
 ui_print "- Installing Box for Magisk/KernelSU"
 ui_print "- Extract the ZIP file and skip the META-INF folder into the $MODPATH folder"
-unzip -o "${ZIPFILE}" -x 'META-INF/*' -d "${MODPATH}" >&2
+unzip -o "$ZIPFILE" -x 'META-INF/*' -d "$MODPATH" >&2
 
 if [ -d "/data/adb/box" ]; then
   ui_print "- Backup box"
@@ -61,8 +59,8 @@ mkdir -p $MODPATH/system/bin/
 mkdir -p /data/adb/box/run/
 
 ui_print "- Extract the files uninstall.sh and box_service.sh into the $MODPATH folder and ${service_dir}"
-unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d "${MODPATH}" >&2
-unzip -j -o "${ZIPFILE}" 'box_service.sh' -d "${service_dir}" >&2
+unzip -j -o "$ZIPFILE" 'uninstall.sh' -d "$MODPATH" >&2
+unzip -j -o "$ZIPFILE" 'box_service.sh' -d "${service_dir}" >&2
 
 ui_print "- Delete leftover files"
 rm -rf $MODPATH/box
@@ -84,39 +82,23 @@ set_perm /data/adb/box/scripts/start.sh  0  0  0755
 # fix "set_perm_recursive /data/adb/box/scripts" not working on some phones.
 chmod ugo+x /data/adb/box/scripts/*
 
-ui_print ""
 ui_print "-----------------------------------------------------------"
-ui_print "- Do you want to download KERNEL and GEOX?"
+ui_print "- Do you want to download KERNEL(xray clash v2fly sing-box) and GEOX(geosite geoip mmdb)? size: ±100MB."
 ui_print "- Make sure you have a good internet connection."
-ui_print "- size ±100MB."
-ui_print "- [ Vol UP: Yes ]"
-ui_print "- [ Vol DOWN: No ]"
+ui_print "- [ Vol UP(+): Yes ] / [ Vol DOWN(-): No ]"
+
 while true ; do
   getevent -lc 1 2>&1 | grep KEY_VOLUME > $TMPDIR/events
-  sleep 1
   if $(cat $TMPDIR/events | grep -q KEY_VOLUMEUP) ; then
     ui_print "- it will take a while...."
-    /data/adb/box/scripts/box.tool all && ui_print "- downloads are complete."
+    /data/adb/box/scripts/box.tool all
     break
   elif $(cat $TMPDIR/events | grep -q KEY_VOLUMEDOWN) ; then
-     ui_print "- skip download KERNEL and GEOX"
-    break
+    ui_print "- skip download KERNEL and GEOX" && break
   fi
 done
 
-for pid in $(${busybox} pidof inotifyd) ; do
-  if grep -q box.inotify /proc/${pid}/cmdline ; then
-    kill -15 ${pid}
-  fi
-done
-
-inotifyd "/data/adb/box/scripts/box.inotify" "/data/adb/modules/box_for_root" > /dev/null 2>&1 &
-
-if [ "$KSU" = true ]; then
-  sed -i "s/name=.*/name=Box for KernelSU/g" "${MODPATH}/module.prop"
-else
-  sed -i "s/name=.*/name=Box for Magisk/g" "${MODPATH}/module.prop"
-fi
+[ "$KSU" = "true" ] && sed -i "s/name=.*/name=Box for KernelSU/g" $MODPATH/module.prop || sed -i "s/name=.*/name=Box for Magisk/g" $MODPATH/module.prop
 
 ui_print "- Installation is complete, reboot your device"
 ui_print "- report issues to @taamarin on Telegram"
