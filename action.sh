8cff72c9ce259f7f9446a89d7d18125b166bf533
雪狐 <mengshouer@outlook.com>
2024-11-02 08:15:22 +0800

    refactor: action 使用 pid 判断服务是否开启 (#153)

--- action.sh
+++ action.sh
@@ -1,15 +1,16 @@
 #!/system/bin/sh
-BOXDIR=/data/adb/box
 
-if [ ! -e "/$BOXDIR/started" ]; then
-    echo "正在启动服务，请耐心等待…"
-    echo "Service is starting,please wait for a moment"
-    su -c /$BOXDIR/scripts/box.service start && su -c /$BOXDIR/scripts/box.iptables enable
-    touch "/$BOXDIR/started"
-    exit
-else
+box_dir=/data/adb/box
+box_run="${box_dir}/run"
+box_pid="${box_run}/box.pid"
+
+if [ -f "${box_pid}" ]; then
     echo "正在关闭服务…"
     echo "Service is shutting down"
-    su -c /$BOXDIR/scripts/box.iptables disable && su -c /$BOXDIR/scripts/box.service stop
-    rm "/$BOXDIR/started"
+    su -c $box_dir/scripts/box.iptables disable && su -c $box_dir/scripts/box.service stop
+else
+    echo "正在启动服务，请耐心等待…"
+    echo "Service is starting,please wait for a moment"
+    su -c $box_dir/scripts/box.service start && su -c $box_dir/scripts/box.iptables enable
+    exit
 fi
